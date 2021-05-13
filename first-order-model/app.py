974621f1d88ea9f61187dc4cf4cd9f8b6b55dcf1
Nhat Nam Nguyen <67188030+henrynamnguyen@users.noreply.github.com>
2021-05-13 16:16:21 -0400

    Update app.py

--- first-order-model/app.py
+++ first-order-model/app.py
@@ -10,8 +10,6 @@ import matplotlib.animation as animation
 from skimage.transform import resize
 import warnings
 warnings.filterwarnings("ignore")
-from demo import load_checkpoints, make_animation
-from skimage import img_as_ubyte
 
 app = Flask(__name__)
 api = Api(app)
@@ -26,38 +24,41 @@ class Upload(Resource):
     def post(self):
         #get the url of users' images
         user_data = request.get_json()
+        user_id = user_data["user_id"]
         image_data = str(user_data["image_url"])
+        song_selected = user_data["song_selected"]
+        
         image = image_data.content
-        f = open("temp.jpg","wb")
-        f.write(image_da)
+        f = open("temp.jpeg","wb")
+        f.write(image)
         f.close()
         
-        #load images and the blueprint videos
-        source_image = imageio.imread("temp.jpg")
-        reader = imageio.get_reader("http://storage.googleapis.com/memekit-blueprint-videos/nhatnam2.mp4")
-        source_image = resize(source_image, (256, 256))[..., :3]
+        reader = imageio.get_reader('nhatnam2.mp4')
+        
+        subprocess.run('python3 demo.py --config config/vox-256.yaml --driving_video nhatnam2.mp4 --source_image temp.jpg --checkpoint /content/drive/MyDrive/first-order-motion-model/vox-cpk.pth.tar --result_video output.mp4 --relative --adapt_scale', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell = True)
+        
+        subprocess.run('ffmpeg -i {} -i {} -c:v copy -c:a aac output2.mp4'.format("output.mp4","14.mp3".format(song_selected)),shell=True)
+        
+        subprocess.run('ffmpeg -i {} -i {} -filter_complex overlay=W-w-5:H-h-5 result.mp4'.format("output2.mp4", "logo.png"),shell=True)
+        
+        client = gcs.Client()
+        
+        bucket_name = "lively-iris-309008.appspot.com"
+        bucket = client.get_bucket(bucket_name) #connect with "memekit" bucket
+        
+        blob_name = "{}/{}".format(user_id, "8")
+        blob = bucket.blob(blob_name)
 
-        fps = reader.get_meta_data()['fps']
-        driving_video = []
-        try:
-            for im in reader:
-                driving_video.append(im)
-        except RuntimeError:
-            pass
-        reader.close()
-        driving_video = [resize(frame, (256, 256))[..., :3] for frame in driving_video]
- 
-        #load checkpoints
-        generator, kp_detector = load_checkpoints(config_path='config/vox-256.yaml', 
-                                checkpoint_path='https://storage.googleapis.com/memekit-checkpoint/vox-cpk.pth.tar')
-        
-        #perform image processing 
-        predictions = make_animation(source_image, driving_video, generator, kp_detector, relative=True)
-        
-        
-        return Response(predictions, mimetype="video/mp4")
+        with open("result.mp4","rb") as g:
+            blob.upload_from_file(g,content_type="video/mp4")
+            
+        os.remove('/content/MemeKit/first-order-model/' + 'temp.jpg')
+        os.remove('/content/MemeKit/first-order-model/' + 'output.mp4')
+        os.remove('/content/MemeKit/first-order-model/' + 'output2.mp4')
+        os.remove('/content/MemeKit/first-order-model/' + 'result.mp4')
 
-api.add_resource(Upload,"/ppload")
+
+api.add_resource(Upload,"/upload")
 
 if __name__ == "__main__":
     app.run(debug=True)
